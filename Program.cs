fbbfc344bf1c5f4d25bd4ec2c016a1da76d03c27
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-02-28 16:51:45 +0100

    feat: funzione statistiche per trovare minimo massimo e media nei vettori

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@
             double[] pesi = new double[dimensione];
             CaricaVettori(ref pesi, ref età);
 
-        }
+            double min = pesi[0];
+            double max = pesi[0];
+            double media = 0.0;
+            Statistiche(ref pesi, ref media, ref min, ref max);
+            Console.WriteLine($"Il peso minimo estratto è: {min}\nIl peso massimo estratto è: {max}\nLa media è {media}");
+    }
         static void CaricaVettori(ref double[] pesi, ref int[] età)
         {
             Random random = new Random();
@@ -19,6 +24,24 @@
                 età[i] = random.Next(18, 100);
                 pesi[i] = random.Next(50, 101);
             }
+        }
+
+        static void Statistiche(ref double[] pesi, ref double media, ref double min, ref double max)
+        {
+            double sommaPesi = 0.0;
+            for (int i = 0; i < pesi.Length; i++)
+            {
+                if (pesi[i] > max)
+                {
+                    max = pesi[i];
+                }
+                if (pesi[i] < min)
+                {
+                    min = pesi[i];
+                }
+                sommaPesi += pesi[i];
+            }
+            media = sommaPesi / (double)pesi.Length;
         }
 
 }
