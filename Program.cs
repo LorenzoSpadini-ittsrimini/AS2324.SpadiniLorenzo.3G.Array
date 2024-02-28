67ea01d51891f3cb91f97d86afc49701425fb6ca
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-02-28 16:53:47 +0100

    feat: funzione ordina per ordinare i vettori per età crescente

--- Program.cs
+++ Program.cs
@@ -15,7 +15,11 @@
             double media = 0.0;
             Statistiche(ref pesi, ref media, ref min, ref max);
             Console.WriteLine($"Il peso minimo estratto è: {min}\nIl peso massimo estratto è: {max}\nLa media è {media}");
-    }
+
+            Ordina(pesi, età);
+            for (int j = 0; j < dimensione; j++)
+                Console.WriteLine($"età:{età[j]} - peso:{pesi[j]}");
+        }
         static void CaricaVettori(ref double[] pesi, ref int[] età)
         {
             Random random = new Random();
@@ -42,6 +46,24 @@
                 sommaPesi += pesi[i];
             }
             media = sommaPesi / (double)pesi.Length;
+        }
+        static void Ordina(double[] pesi, int[] età)
+        {
+            for (int i = 0; i < età.Length - 1; i++)
+            {
+                for (int j = 0; j < età.Length - i - 1; j++)
+                {
+                    if (età[j] > età[j + 1])
+                    {
+                        int tempEtà = età[j];
+                        età[j] = età[j + 1];
+                        età[j + 1] = tempEtà;
+                        double tempPeso = pesi[j];
+                        pesi[j] = pesi[j + 1];
+                        pesi[j + 1] = tempPeso;
+                    }
+                }
+            }
         }
 
 }
