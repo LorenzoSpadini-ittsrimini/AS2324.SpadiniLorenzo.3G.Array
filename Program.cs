cbbea2d9a3e2400eab65cc169fd84a22c33eba7f
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-02-28 16:49:11 +0100

    feat: funzione che carica i vettori età e pesi

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace AS2324.SpadiniLorenzo._3G.Array
-{
+﻿using System;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("inserire il numero di persone:");
+            int dimensione = int.Parse(Console.ReadLine());
+            int[] età = new int[dimensione];
+            double[] pesi = new double[dimensione];
+            CaricaVettori(ref pesi, ref età);
+
         }
-    }
+        static void CaricaVettori(ref double[] pesi, ref int[] età)
+        {
+            Random random = new Random();
+            for (int i = 0; i < età.Length; i++)
+            {
+                età[i] = random.Next(18, 100);
+                pesi[i] = random.Next(50, 101);
+            }
+        }
+
 }
+
+
+
+
