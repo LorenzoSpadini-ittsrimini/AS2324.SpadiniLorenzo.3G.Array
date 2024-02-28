890d849e76184ae5d29d78580248858f61646c12
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-02-28 16:42:46 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2324.SpadiniLorenzo._3G.Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
